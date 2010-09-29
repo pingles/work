4c73094d57174427ecaa13bb2f0b58e0c2ba87fc
Bradford Cross <bradford.n.cross@gmail.com>
2010-09-29 10:12:55 -0700

    caching with restriction to only certain params in the fn's arg list.

--- src/work/cache.clj
+++ src/work/cache.clj
@@ -27,31 +27,48 @@
 (defn new-keys [m ks]
   (filter #(not (contains-key? m %)) ks))
 
-(defn put-res [m f args & exp]
-  (let [res (apply f args)
-	_ (.put m args (if (not exp) res
-			{:at (time/now) :exp exp :val res}))]
-    res))
+(defn put-exp
+  "behaves like normal put if there is not expire time, otherwise, it will schedule the element of the map to be deleted once the time expires."
+  [m k v & exp]
+  (.put m k (if (not exp) v
+		{:at (time/now) :exp exp :val v})))
+
+(defmacro only [bindings]
+  `(let [bdg# (map (fn [bind#] (if (not (= "_" (name bind#)))
+				 true false))
+		   '~bindings)]
+     (fn [args#] (let [~bindings args#]
+		   (filter identity (map
+				     (fn [v# keep#]
+				       (if keep#
+					 v# nil))
+				     ~bindings
+				     bdg#))))))
 
 (defn cache
-  "wraps a function f in an (optionally) expiring cache memoizer.  Optionally takes expire, a number of seconds before this item will expire from the cache."
+  "wraps a function f in an (optionally) expiring cache.  Expire is the number of seconds before this item will expire from the cache."
   ([f] (cache f :never))
   ([f expire] (cache (cache-map) f expire))
-  ([m f expire]
-  (fn [& args]
-    (let [m (cache-map)
-	  cached (contains-key? m args)
-	  expires? (not (= :never expire))]
-      (cond
-       (and cached expires?)
-       (let [{at :at  exp :exp v :val} (get m args)]
-	 (if (> (time/in-secs (time/interval at (time/now))) exp)
-	   (put-res m f args expire)
-	   v))
-       cached (get m args)
-       (and (not cached) expires?)
-       (put-res m f args expire)
-       :else
-	 (let [res (apply f args)
-	     _ (put m args res)]
-	 res))))))
+  ([m f expire] (cache m f expire identity))
+  ([m f expire cache-on]
+     (fn [& args]
+       (let [cache-args (cache-on args) 
+	     cached (contains-key? m cache-args)
+	     expires? (not (= :never expire))]
+	 (cond
+	  (and cached expires?)
+	  (let [{at :at  exp :exp v :val} (get m cache-args)]
+	    (if (> (time/in-secs (time/interval at (time/now))) exp)
+	      (let [res (apply f args)
+	 	    _ (put-exp m cache-args res expire)]
+	 	res)
+	      v))
+	  cached (get m cache-args)
+	  (and (not cached) expires?)
+	  (let [res (apply f args)
+	 	_ (put-exp m cache-args res expire)]
+	    res)
+	  :else
+	  (let [res (apply f args)
+	 	_ (put m cache-args res)]
+	    res))))))
---
+++ test/work/cache_test.clj
@@ -0,0 +1,20 @@
+(ns work.cache-test
+  (:use work.cache
+	clojure.test))
+
+(deftest only-args
+  (is (= [2 4 5]
+	 ((only [_ b _ d e])
+	  [1 2 3 4 5]))))
+
+(deftest wrong-cache
+  (let [m (cache-map)
+	c (cache m 
+		 + ;;caching plus fn
+		 :never ;;never expires
+		 (only [a _ c]))] ;;only cache on first and third args
+    (is (= 15 (c 4 5 6)))
+    (is (contains-key? m [4 6]))
+    ;;first call cached [4 6]
+    ;;next call is now broken becasue middle slot is not 5, new total sould be 20. cache works.
+    (is (= 15 (c 4 10 6)))))
