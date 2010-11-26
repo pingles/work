5cc9eed9d6d818a8230475c3fc96a0d0f84bc92b
Matt Revelle <mrevelle@gmail.com>
2010-11-26 17:23:22 -0500

    Updated version of Clojure dep.

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
 (defproject work "0.0.1-SNAPSHOT"
   :description "Clojure workers."
   :url "http://github.com/clj-sys/work"
-  :dependencies [[org.clojure/clojure "1.2.0-master-SNAPSHOT"]
-                 [org.clojure/clojure-contrib "1.2.0-SNAPSHOT"]
-		 [clj-serializer "0.1.0"]
-		 [clj-time "0.1.0-RC1"]
-		 [clj-json "0.3.0-SNAPSHOT"]]
+  :dependencies [[org.clojure/clojure "1.2.0"]
+                 [org.clojure/clojure-contrib "1.2.0"]
+                 [clj-serializer "0.1.0"]
+                 [clj-time "0.1.0-RC1"]
+                 [clj-json "0.3.0-SNAPSHOT"]]
   :dev-dependencies [[swank-clojure "1.3.0-SNAPSHOT"]
-		     [lein-clojars "0.5.0"]])
+                     [lein-clojars "0.5.0"]])
