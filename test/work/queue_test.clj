1034f14d6360c9e9cccb49451b33ae5f121de40a
Bradford <bradford@measuringmeasures.com>
2010-12-03 21:46:52 -0800

    adding a message queue tests.

--- test/work/queue_test.clj
+++ test/work/queue_test.clj
@@ -8,3 +8,10 @@
 	_ (work/offer q "b")
 	_ (work/offer-unique q "a")]
     (is (= 2 (work/size q)))))
+
+(deftest message-queue-test
+  (let [q (work/local-queue)
+	_ (work/offer-msg q [1 2 3])
+	_ (work/offer-msg q 3 4 5)]
+    (is (= [1 2 3] (work/poll-msg q)))
+    (is (= [3 4 5] (work/poll-msg q)))))
