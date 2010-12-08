b0e1aff7020712f63ff6d3ce12cf6161b7965e8a
Matt Revelle <mrevelle@gmail.com>
2010-12-08 13:58:33 -0500

    Formatting.

--- project.clj
+++ project.clj
@@ -4,9 +4,9 @@
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
                  [clj-serializer "0.1.1"]
-		 [clj-sys/plumbing "0.1.3-SNAPSHOT"]
+                 [clj-sys/plumbing "0.1.3-SNAPSHOT"]
                  [clj-time "0.2.0-SNAPSHOT"]
                  [clj-json "0.3.1"]]
   :dev-dependencies [[swank-clojure "1.3.0-SNAPSHOT"]
                      [lein-clojars "0.5.0"
-		      lein-run "1.0.0"]])
+                      lein-run "1.0.0"]])
